c61359475f531597c050ea43a81f67162a56951d
jahandar <axiyev.cahandar@gmail.com>
2024-04-03 22:33:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
+///////
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+/////
+
+
+app.listen(8800, () => {
+    console.log("server is ready now");
+});
